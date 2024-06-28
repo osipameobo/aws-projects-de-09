2a39fd6d2cb3f6d6db52ae5b3dd473edfb5bdac2
osipameobo <omuemuesther@gmail.com>
2024-06-28 10:46:43 +0200

    first project flask01-02 added

---
+++ projects/flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def head():
+     return render_template('index.html', number1=35, number2=36)
+
+@app.route('/multi')
+def number():
+     return render_template('body.html', value1=x, value2=y, sum=x*y)
+
+
+if __name__=="__main__":
+    # app.run(debug=True)
+      app.run(host='0.0.0.0', port=80)
+
+
+
+
+
+
